3b28762d830e9b36339fe9d7a01f2ec512443bcc
Lions <zoac@outlook.it>
2023-12-07 16:27:03 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const caselle = function () {
+const caselleDiv = document.getElementById('tabellone')
+for (let i = 0; i < 90; i++) {
+    const casellaDiv = document.createElement('div')
+    casellaDiv.classList.add('casellaSingola')
+    const cellValue = document.createElement('h3')
+    cellValue.innerText = i + 1
+    caselleDiv.appendChild(cellValue)
+    casellaDiv.addEventListener('click', function (e) {
+
+    })
+}}
+caselle()
+const formReference = document.getElementsByTagName('form')[0]
+formReference.addEventListener('submit', function (e) {
+  e.preventDefault()})
+
+  let numbers = function () {
+      for (let i = 1; i < 91; i++) {
+        numbers = Math.floor(Math.random(1) * 90)
+        
+    }
+  }
